4f33e7204294246b4bae6550e2d5606a36c9a2f9
britza <bzegarraflores@danielcastelao.org>
2021-05-17 18:12:08 +0200

    comandos añadidos a Dispatcher

--- src/main/kotlin/Dispatcher.kt
+++ src/main/kotlin/Dispatcher.kt
@@ -1,8 +1,11 @@
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.callbackQuery
 import com.github.kotlintelegrambot.dispatcher.command
 import com.github.kotlintelegrambot.dispatcher.message
 import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
+import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
 import com.github.kotlintelegrambot.extensions.filters.Filter
 import com.github.kotlintelegrambot.network.fold
 import java.lang.Compiler.command
@@ -16,7 +19,7 @@ fun main (){
         dispatch {
 
             command("hola"){
-                val result=bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text= "Buenos dias, pinche pendeja")
+                val result=bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text= "Buenos dias, pinche pendejo")
                 result.fold ({ },{ })
 
             }
@@ -32,10 +35,53 @@ fun main (){
                 result.fold({},{})
             }
 
+            command("muricion"){
+                val result=bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Toma gancho y cuerda, si miras por la ventana ahí tienes un arbol")
+                result.fold({},{})
+            }
+
+            command("deprimision"){
+                val result=bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Si quieres llorar, tienes puerta abierta a la llorecry")
+                result.fold({},{})
+            }
+
+
             message(Filter.Reply or Filter.Forward){
                 bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text= "No me respondas idiota, no ves que mi tiempo es valioso")
 
 
+            }
+
+            message (Filter.Sticker){
+                bot.sendMessage(ChatId.fromId(message.chat.id), text = "A ver amigo, que mierda de sticker me envias, ¿enserio me haces perder mi tiempo por esto?")
+            }
+
+            command("elige"){
+                val inlineKeyboardMarkup = InlineKeyboardMarkup.create(
+
+                    listOf(InlineKeyboardButton.CallbackData(text ="Tocame ", callbackData = "Picantee ( ͡° ͜ʖ ͡°)")),
+                    listOf(InlineKeyboardButton.CallbackData(text = "Pinchame",callbackData = "¿UwU?"))
+
+                )
+                bot.sendMessage(
+                    chatId = ChatId.fromId(message.chat.id),
+                    text = "Presioname se que quieres",
+                    replyMarkup = inlineKeyboardMarkup
+                )
+            }
+
+            callbackQuery("Picantee ( ͡° ͜ʖ ͡°)") {
+                val chatId = callbackQuery.message?.chat?.id ?: return@callbackQuery
+                bot.sendMessage(ChatId.fromId(chatId), callbackQuery.data)
+            }
+
+            callbackQuery(
+                callbackData = "¿UwU?",
+                callbackAnswerText = "Vamos,Se que quieres ",
+                callbackAnswerShowAlert = true
+            ) {
+                val chatId = callbackQuery.message?.chat?.id ?: return@callbackQuery
+                bot.sendMessage(ChatId.fromId(chatId), callbackQuery.data)
             }
         }
 
